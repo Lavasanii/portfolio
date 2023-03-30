91e93e812be55ac904fca3f5611cb313f79dbf20
Lavasanii <saeid.f.lavasani@gmail.com>
2023-03-30 20:05:44 +0200

    Add docDeploy and PorjectRB

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/projectRB', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'projectRB.html'));
+});
 
 
 app.listen(port, () => {
