cdf0851244ec885cb4d57708dc683afa2e2adf6e
Mohammad Malmir <44100867+massimiliano3100@users.noreply.github.com>
2023-07-15 19:12:08 +0200

    Add Mause Icon to Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <S1avatarWithBackground></S1avatarWithBackground>
+  <img :src="mouseIcon" class="rounded mx-auto d-block my-4" alt="mouse-icon" />
 </template>
 
 <script setup lang="ts">
 import S1avatarWithBackground from "../components/home-components/S1avatarWithBackground.vue";
+import mouseIcon from "../../public/icons/mouse-icon.svg";
 </script>
 
 <style lang="scss" scoped></style>
