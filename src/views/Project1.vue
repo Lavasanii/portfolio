ee847450861e4fbf5338c806829af89a99e4046a
Lavasanii <saeid.f.lavasani@gmail.com>
2023-07-27 23:04:34 +0200

    Fixed Project background text

--- src/views/Project1.vue
+++ src/views/Project1.vue
@@ -12,7 +12,7 @@
     <div class="h3 overview-text text-uppercase">Overview</div>
     <div class="margin-between-sections" v-for="s in section1Items" :key="s.id">
       <div class="sections-title">{{ s.title }}</div>
-      <div class="sections-texts">{{ s.text }}</div>
+      <div class="sections-texts"><span v-html="s.text"></span></div>
     </div>
   </div>
   <Project1Process class="margin-between-sections"></Project1Process>
@@ -44,12 +44,18 @@ interface sectionsInterfaces {
 const section1Items = ref<sectionsInterfaces[]>([
   {
     id: 0,
-    title: "Projekthintergründ",
-    text: "Durch meinegroße Leidenschaft für Coding habe ich mich für dieses Project entschieden, daheutzutage Learning Plattformen sehrbliebt sind , entschteht eine große konkurrenz zwischen denen. Die jungere Leute , oder Die Stundent wollten schnell die Möglichkeiten, bzw. was dieWebsite zu Bieten hat, vergleichen, Hier kann eine Saubere UI sehr Hilfreich sein.Rubrik’sCode ist eine kleine StartUp , die durch ein erfahrener Programmier gegründetist. Der versuchte das Maschine Learning (ML) schneller und diskreter zumlehren und dazu hat er eine Methode entwickelt, die Mann die Schritte durch eineJernourny lernen kann.Die Websiteist durch zwei Hauptplan verteiltet. Der erste ist das Learning von ML mitPython durch die Bücher und der zweite Teil ist das Lernen von ML mit C# durchVideos. Es wurde dabei so gedacht, dass man die Schritte weitergehen soll unddabei kann man es sehr gut lernen. Die Lectures sind so eingeordnet dass manerst mal die Grundlage wie z.B: Mathematik für ML lernt und danach lernt man dieCoding und weitere Schritte.",
+    title: "Project background",
+    text: `I decided to undertake this project due to my passion for coding. The popularity of learning platforms nowadays has led to intense competition amongst them. Younger people and students, in particular, desire a quick comparison of website options and offerings. Having a clear and user-friendly user interface can be exceedingly helpful in this regard. 
+    <br />
+    Rubrik's Code is a small startup established by an experienced software developer. Our aim is to accelerate and optimize Machine Learning (ML) education. To achieve this goal, our unique approach makes the steps in the learning process more understandable.
+    <br />
+    Our website is divided into two primary sections. Part one focuses on learning machine learning with Python using textbooks. Part two consists of lessons on learning machine learning with C# through videos. The structure is designed to ensure that each step builds on the previous one and provides a deeper understanding.
+    <br />
+    The lessons are carefully designed to first introduce essential concepts, such as Mathematics for Machine Learning, and then proceed to coding and more advanced steps. This approach ensures that learning is efficient and practical, and allows for a step-by-step building process.`,
   },
   {
     id: 1,
-    title: "The Problem",
+    title: "Problem",
     text: "Durch meinegroße Leidenschaft für Coding habe ich mich für dieses Project entschieden, daheutzutage Learning Plattformen  sehrbliebt sind , entschteht eine große konkurrenz zwischen denen. Die jungere Leute , oder Die Stundent wollten schnell die Möglichkeiten, bzw. was dieWebsite zu Bieten hat, vergleichen, Hier kann eine Saubere UI sehr Hilfreich sein.Rubrik’sCode ist eine kleine StartUp , die durch ein erfahrener Programmier gegründetist. Der versuchte das Maschine Learning (ML) schneller und diskreter zumlehren und dazu hat er eine Methode entwickelt, die Mann die Schritte durch eineJernourny lernen kann.Die Websiteist durch zwei Hauptplan verteiltet. Der erste ist das Learning von ML mitPython durch die Bücher und der zweite Teil ist das Lernen von ML mit C# durchVideos. Es wurde dabei so gedacht, dass man die Schritte weitergehen soll unddabei kann man es sehr gut lernen. Die Lectures sind so eingeordnet dass manerst mal die Grundlage wie z.B: Mathematik für ML lernt und danach lernt man dieCoding und weitere Schritte.",
   },
   {
