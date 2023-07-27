7e5dbe2677433f67874f49e95a453144c8f07074
Lavasanii <saeid.f.lavasani@gmail.com>
2023-07-27 20:16:36 +0200

    Anpassung Maus Logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <AvatarWithBackground></AvatarWithBackground>
-  <img :src="mouseIcon" class="rounded mx-auto d-block my-5" alt="mouse-icon" />
+  <div class="mousey mx-auto d-block my-5">
+    <div class="scroller"></div>
+  </div>
   <div class="container mb-5">
     <p class="h3">Projekte</p>
   </div>
@@ -10,7 +12,55 @@
 <script setup lang="ts">
 import AvatarWithBackground from "../components/home-components/AvatarWithBackground.vue";
 import Projects from "../components/home-components/Projects.vue";
-import mouseIcon from "../assets/icons/mouse-icon.svg";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.scroll-downs {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+
+  width: 57px;
+  height: 55px;
+}
+
+.mousey {
+  width: 2px;
+  padding: 10px 15px;
+  height: 32px;
+  border: 2px solid #9D9D9D;
+  border-radius: 25px;
+  opacity: 0.75;
+  box-sizing: content-box;
+}
+
+.scroller {
+  width: 2.5px;
+  height: 10px;
+  border-radius: 25%;
+  background-color: #5f5e5ecd;
+  animation-name: scroll;
+  animation-duration: 2.2s;
+  animation-timing-function: cubic-bezier(.15, .41, .69, .94);
+  animation-iteration-count: infinite;
+}
+
+@keyframes scroll {
+  0% {
+    opacity: 0;
+  }
+
+  10% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translateY(15px);
+    opacity: 0;
+  }
+}
+</style>
