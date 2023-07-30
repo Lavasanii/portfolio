37fefcf8e652e9573ac29327e23cc3a1140fb3d8
Mohammad Malmir <44100867+massimiliano3100@users.noreply.github.com>
2023-07-30 20:02:44 +0200

    About mee mit Bug

--- src/views/Aboutme.vue
+++ src/views/Aboutme.vue
@@ -1,7 +1,35 @@
 <template>
-    <h1> Hiiiiiiiiiiiii </h1>
+  <div style="width: 600px; background-color: red; position: relative">
+    <div class="d-flex justify-content-end" style="position: relative">
+      <img
+        :src="saeidImageAboutme"
+        :alt="saeidImageAboutme"
+        class="rounded float-left"
+        style="max-width: 100%; height: auto"
+      />
+      <div
+        style="
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          z-index: 1;
+        "
+      >
+        <p style="background-color: white; padding: 10px">Hiii</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import saeidImageAboutme from "../../src/assets/images/saeidImageAboutme.svg";
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+/* Hier können Sie das Styling des Texts anpassen */
+p {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
