5d527a29883a3895ba2c87452975b642c4e91632
Mohammad Malmir <44100867+massimiliano3100@users.noreply.github.com>
2023-07-30 19:19:14 +0200

    Add Mail Icon in Footer and About me

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer pt-5 pb-2" style="margin-top: 80px;">
+  <footer class="footer pt-5 pb-2" style="margin-top: 80px">
     <div class="container-fluid">
       <div class="row justify-content-center">
         <div class="col my-2">
@@ -7,7 +7,13 @@
             &copy; {{ new Date().getFullYear() }} - Saeid Farajollahlavasani
           </p>
           <div class="col my-4">
-            <a v-for="s in socialItems" :key="s.id" :href="s.linkToSocial" target="_blank" class="mx-2">
+            <a
+              v-for="s in socialItems"
+              :key="s.id"
+              :href="s.linkToSocial"
+              target="_blank"
+              class="mx-2"
+            >
               <img :src="s.iconSrc" :alt="s.name" width="25" height="25" />
             </a>
           </div>
@@ -20,9 +26,9 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import linkedin from "../assets/icons/linkedin.svg"
-import Instagram from "../assets/icons/Instagram.svg"
-
+import linkedin from "../assets/icons/linkedin.svg";
+import Instagram from "../assets/icons/Instagram.svg";
+import mail from "../assets/icons/mail.svg";
 
 interface socialItemsInterface {
   id: number;
@@ -43,6 +49,12 @@ const socialItems = ref<socialItemsInterface[]>([
     name: "Instagram",
     iconSrc: Instagram,
     linkToSocial: "https://www.instagram.com/saeid_lavasani/",
+  },
+  {
+    id: 3,
+    name: "Mail",
+    iconSrc: mail,
+    linkToSocial: "mailto:s_f_lavasani@yahoo.com",
   },
 ]);
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Project1 from "../views/Project1.vue";
+import Aboutme from "../views/Aboutme.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/rubrik-project",
       name: "Project1",
       component: Project1,
+    },
+    {
+      path: "/aboutme",
+      name: "aboutme",
+      component: Aboutme,
     },
   ],
 });
